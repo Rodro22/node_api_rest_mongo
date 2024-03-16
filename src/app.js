cb998501a3baa1f5b8e059f4a29ce904602fa886
Rodro22 <rodrigoe.lezcano@gmail.com>
2024-03-15 21:29:16 -0300

    Servidor

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { config } = require('dotenv')
+const bodyParser = require('body-parser')
+config()
+
+const bookRoutes = require('./routes/book.routes')
+
+//usamos express para los middleware
+const app = express();
+
+app.use(bodyParser.json()) //Parseador de Bodies
+
+//Aca conectaremos la BD:
+mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DB_NAME})
+const db = mongoose.connection;
+
+app.use('/books', bookRoutes)
+
+
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en ${port}`)
+})
+
+
+
+
